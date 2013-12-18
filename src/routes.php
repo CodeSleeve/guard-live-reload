3d810eebdfb83a5836431cbbe1a399bdbe134fdc
Kelt Dockins <kelt@dockins.org>
2013-12-18 02:07:07 -0600

    fixing routes

--- src/routes.php
+++ src/routes.php
@@ -19,15 +19,7 @@ Route::group(Config::get('watcher-live-reload::routing'), function()
 
 	Route::get('ws', function()
 	{
-		list($resource,$host,$origin) = get_headers($buffer);
-		$upgrade = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-		           "Upgrade: WebSocket\r\n" .
-		           "Connection: Upgrade\r\n" .
-		           "WebSocket-Origin: " . $origin . "\r\n" .
-		           "WebSocket-Location: ws://" . $host . $resource . "\r\n" .
-		           "\r\n";
-		$handshake = true;
-		socket_write($socket,$upgrade.chr(0),strlen($upgrade.chr(0)));
+		
 	});
 
 });
