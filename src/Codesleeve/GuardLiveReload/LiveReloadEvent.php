a687abbe0ff2ba9930091c8b14186f6ca2c7fc64
Kelt Dockins <kelt@dockins.org>
2013-12-19 21:22:32 -0600

    adding stuff

--- src/Codesleeve/GuardLiveReload/LiveReloadEvent.php
+++ src/Codesleeve/GuardLiveReload/LiveReloadEvent.php
@@ -1,6 +1,5 @@
 <?php namespace Codesleeve\GuardLiveReload;
 
-use DateTime;
 use Codesleeve\Guard\Events\EventInterface;
 
 class LiveReloadEvent implements EventInterface
@@ -8,12 +7,12 @@ class LiveReloadEvent implements EventInterface
 	/**
 	 * We have started the watcher... 
 	 * 
-	 * @param  [type] $watcher [description]
+	 * @param  [type] $guard [description]
 	 * @return [type]          [description]
 	 */
-	public function start($watcher)
+	public function start($guard)
 	{
-		$this->watcher = $watcher;
+		$this->watcher = $guard;
 	}
 
 	/**
@@ -30,12 +29,12 @@ class LiveReloadEvent implements EventInterface
 	 * Let livereload server know to send updates
 	 * 
 	 * @param  $event  
-	 * @param  $watcher
 	 * @return         
 	 */
 	public function listen($event)
 	{
-	 	$date = new DateTime;
-		file_put_contents(storage_path() . '/reload_trigger_file', $date->format(DateTime::ISO8601));
+	 	// 	$date = new DateTime;
+		// file_put_contents(storage_path() . '/reload_trigger_file', $date->format(DateTime::ISO8601));
+		// 
 	}
 }
