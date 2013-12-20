83ca76821cd61e4d585b51f0a01002c177a12c27
Kelt Dockins <kelt@dockins.org>
2013-12-19 20:33:02 -0600

    fixing up live reload for guard

--- src/Codesleeve/GuardLiveReload/LiveReloadEvent.php
+++ src/Codesleeve/GuardLiveReload/LiveReloadEvent.php
@@ -1,17 +1,39 @@
 <?php namespace Codesleeve\GuardLiveReload;
 
 use DateTime;
+use Codesleeve\Guard\Events\EventInterface;
 
-class LiveReloadEvent
+class LiveReloadEvent implements EventInterface
 {
 	/**
-	 * let the route for this package know that we should reload now
+	 * We have started the watcher... 
+	 * 
+	 * @param  [type] $watcher [description]
+	 * @return [type]          [description]
+	 */
+	public function start($watcher)
+	{
+		$this->watcher = $watcher;
+	}
+
+	/**
+	 * Stop things
+	 * 
+	 * @return [type]          [description]
+	 */
+	public function stop()
+	{
+
+	}
+
+	/**
+	 * Let livereload server know to send updates
 	 * 
 	 * @param  $event  
 	 * @param  $watcher
 	 * @return         
 	 */
-	public function listen($event, $watcher)
+	public function listen($event)
 	{
 	 	$date = new DateTime;
 		file_put_contents(storage_path() . '/reload_trigger_file', $date->format(DateTime::ISO8601));
