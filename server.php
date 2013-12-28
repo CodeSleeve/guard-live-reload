a4ad42e96e0359ddaf6a7799413993fea7d9d784
Kelt Dockins <kelt@dockins.org>
2013-12-28 13:58:32 -0600

    adding the composer autoloader to server.php

--- server.php
+++ server.php
@@ -1,6 +1,35 @@
 <?php 
 
-require __DIR__ . '/vendor/autoload.php';
+/**
+ * Try to find the composer autoloader and then require it.
+ * If we cannot find the composer autoloader then we will 
+ * throw an Exception
+ * 
+ * @return composer autoloader | Exception
+ */
+function require_composer()
+{
+	$autoloaders = array(
+		__DIR__ . '/../../autoload.php',
+		__DIR__ . '/vendor/autoload.php',
+	);
 
+	foreach ($autoloaders as $autoloader)
+	{
+		$file = realpath($autoloader);
+
+		if (file_exists($file))
+		{
+			return require($file);
+		}
+	}
+
+	throw new Exception("Could not require composer autoloader!");
+}
+
+// require composer's autoloader
+require_composer();
+
+// start a new Live Reload Server
 $server = new Codesleeve\GuardLiveReload\LiveReloadServer;
 $server->start();
